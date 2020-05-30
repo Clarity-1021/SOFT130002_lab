de41034fbd8ba37e3fb887e168a321868181c03d
wendy <798098945@qq.com>
2020-05-30 15:22:46 +0800

    修改exercise1

--- lab9/lab9-exercise1.php
+++ lab9/lab9-exercise1.php
@@ -9,13 +9,26 @@
 <h1>PHP section (inside &lt;?php ... ?&gt; tags)</h1>
 <?php
 //this is a php comment IN tags (will not appear)
-// $d = date("M dS, Y");
-// $d = date("l，M dS ，Y G:i:s"); 
-// echo "This page was generated: " . $d . "<hr/>";
-// $remaining = 366 - date("z");
-$date = date("M dS, Y");
-echo "This page was generated: " . $date . "<hr/>";
-// echo "There are ". $remaining . " days left in the year";
+ $d = date("M dS, Y");
+ $d = date("l，M dS ，Y G:i:s");
+ echo "This page was generated: " . $d . "<hr/>";
+
+$remaining = 365 - date("z");
+
+$year = date("Y");
+if($year % 100 != 0){
+    if($year % 4 == 0){
+        $remaining++;
+    }
+}
+else{
+    if($year % 400 == 0){
+        $remaining++;
+    }
+}
+//$date = date("M dS, Y");
+//echo "This page was generated: " . $date . "<hr/>";
+ echo "There are ". $remaining . " days left in the year";
 echo "<br>"; //notice we must echo tags in php.
 
 ?>
